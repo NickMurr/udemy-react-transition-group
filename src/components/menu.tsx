55e1f8d81f5cfb85807357ce78c58a2d28f18515
nick.murr <nikitosmuravyov@gmail.com>
2019-04-12 13:56:46 +0300

    menu-button

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -4,25 +4,46 @@ import CssTransition from "react-transition-group/CSSTransition";
 
 const Menu = () => {
   const [showBallon, useShowBallon] = useState(false);
+  const [highlitedMenuItem, useHighlitedMenuItem] = useState(false);
+
   const toggle = () => useShowBallon(!showBallon);
+
+  const toggleHighlightedMenuItem = () => {
+    useHighlitedMenuItem(!highlitedMenuItem);
+  };
   return (
-    <div className="container">
-      <button
-        className={cx("toggler", { "toggler--active": showBallon })}
-        onClick={toggle}
-      >
-        Menu
-      </button>
-      {showBallon && (
-        <div className="menu">
-          <ul className="list">
-            <li className="list-item">Home</li>
-            <li className="list-item">Profile</li>
-            <li className="list-item">favourites</li>
-            <li className="list-item">Sign Out</li>
-          </ul>
-        </div>
-      )}
+    <div className="menu-component">
+      <div className="container">
+        <button
+          className={cx("toggler", { "toggler--active": showBallon })}
+          onClick={toggle}
+        >
+          Menu
+        </button>
+        <CssTransition
+          in={showBallon}
+          timeout={350}
+          classNames="balloon"
+          unmountOnExit={true}
+          onEntered={toggleHighlightedMenuItem}
+          onExit={toggleHighlightedMenuItem}
+        >
+          <div className="menu">
+            <ul className="list">
+              <li className="list-item">Home</li>
+              <li
+                className={cx("list-item", {
+                  "list-item--active": highlitedMenuItem,
+                })}
+              >
+                Profile
+              </li>
+              <li className="list-item">Favourites</li>
+              <li className="list-item">Sign Out</li>
+            </ul>
+          </div>
+        </CssTransition>
+      </div>
     </div>
   );
 };
